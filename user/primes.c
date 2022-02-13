a5d8dd3ef39e97e9972faa3190efb6ebc032de22
khy <khydom@qq.com>
2022-02-13 16:53:28 +0800

    add some comments about primes

--- user/primes.c
+++ user/primes.c
@@ -1,7 +1,6 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-
 void redirect(int opt, int pipe[])
 {
     close(opt);     // close std
@@ -13,22 +12,22 @@ void redirect(int opt, int pipe[])
 void filter(int prime)
 {
     int num;
-    while (read(0, &num, 4))//in the end,all process block here
+    while (read(0, &num, 4)) // in the end,all process block here
     {
         if (num % prime != 0)
         {
             write(1, &num, sizeof(num));
         }
     }
-    close(1);//if no data and still write end,process will block in read;close write end in time to avoid this
-    //inform next process to close
+    close(1); // if no data and still write end,process will block in read;close write end in time to avoid this
+    // inform next process to close
 }
 void wait_prime()
 {
     int pid[2]; // curr to next process
     pipe(pid);
     int prime;
-    if (read(0, &prime, sizeof(prime)))//sleep if no data or opening write end
+    if (read(0, &prime, sizeof(prime))) // sleep if no data or opening write end
     {
 
         printf("prime %d\n", prime); // print to 1(default stdout)
@@ -36,14 +35,14 @@ void wait_prime()
         if (fork() == 0)
         {
             redirect(0, pid);
-            wait_prime();
+            wait_prime(); // create next process
             exit(0);
         }
         else
         {
             redirect(1, pid);
             filter(prime);
-            wait(0);//keep the pipeline complete to let process exit after wait_prime()
+            wait(0); // keep the pipeline complete to let process exit after wait_prime()
         }
     }
 }
@@ -65,10 +64,9 @@ int main(int argc, char *argv[])
         {
             write(1, &i, sizeof(i)); // all numbers to pipeline
         }
-        close(1);//in the end,close the first write end to help close all in pipeline;unblock process left to right
-        wait(0);//wait for all outputs to be printed;wait:only if one of its child exits,it coutinues
+        close(1); // in the end,close the first write end to help close all in pipeline;unblock process left to right
+        wait(0);  // wait for all outputs to be printed;wait:only if one of its child exits,it coutinues
         exit(0);
     }
     exit(0);
-
 }
